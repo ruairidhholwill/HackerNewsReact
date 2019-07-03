e9efe8653e38dc9a6f9eca64d7b6b8b31e4f8971
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-03 15:13:32 +0100

    can display date and time

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React, {Fragment} from "react"
 import StoryList from './StoryList'
 
 const Story = (props) => {
     const storyData = props.storiesData.map(story => {
+        const ts = new Date(story.time*1000)
+        console.log(ts)
         return(
-            <div>
+            <Fragment>
             <li key={story.id}><a href={story.url}>{story.title}</a></li>
-            <h4>{story.score}</h4>
-            </div>
+            <h4>{ts.toLocaleString()}</h4>
+            </Fragment>
         )
     })
 
